735e61f1f4b019725097cd0bd51a67f55f93ed76
Pratik Badwaik <pratikbadwaik737@gmail.com>
2026-06-26 09:26:45 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ["https://stockmate-app.vercel.app", "https://jwt-auth-app.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
